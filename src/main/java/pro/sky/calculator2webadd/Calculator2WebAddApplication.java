5db7ea9b9721d85f2fba15fc91d7831890ba2b55
Галина <g416@mail.ru>
2023-08-06 17:42:41 +0500

    проект создан

---
+++ src/main/java/pro/sky/calculator2webadd/Calculator2WebAddApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.calculator2webadd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculator2WebAddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Calculator2WebAddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/calculator2webadd/Calculator2WebAddApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.calculator2webadd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculator2WebAddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
